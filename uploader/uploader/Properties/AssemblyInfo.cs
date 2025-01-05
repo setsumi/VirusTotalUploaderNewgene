869ccd87bf23600ef41417cc3febca13f9aca8a4
setsumi <setsumi@users.noreply.github.com>
2025-01-06 09:56:12 +1000

    version info

--- uploader/uploader/Properties/AssemblyInfo.cs
+++ uploader/uploader/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("uploader")]
-[assembly: AssemblyDescription("Open-Source Winforms application for uploading files to VirusTotal ")]
+[assembly: AssemblyTitle("VirusTotal Uploader Newgene by furniture")]
+[assembly: AssemblyDescription("")]
 #if (DEBUG)
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-[assembly: AssemblyCompany("Samuel Tulach")]
-[assembly: AssemblyProduct("VirusTotalUploader")]
-[assembly: AssemblyCopyright("Copyright © 2021 Samuel Tulach")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VirusTotalUploaderNewgene")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b5329de8-8cd7-4b08-9e87-26778404c7a5")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
